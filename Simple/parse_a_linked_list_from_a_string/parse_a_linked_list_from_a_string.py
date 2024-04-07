e9cba58e0eceb14ff3fab28af4c2194f4d47f751
Daria Hannochenko <hannochenko.pn@ucu.edu.ua>
2024-04-07 18:32:57 +0300

    Implemented parse a linked list from a string

--- Simple/parse_a_linked_list_from_a_string/parse_a_linked_list_from_a_string.py
+++ Simple/parse_a_linked_list_from_a_string/parse_a_linked_list_from_a_string.py
@@ -1,8 +1,5 @@
-
-
-
 class Node:
-    def __init__(self, data, next=None): 
+    def __init__(self, data, next=None):
         self.data = data
         self.next = next
 
@@ -10,41 +7,19 @@ class Node:
 def linked_list_from_string(node_string):
     """The function creates linked list
     from a such string '1 -> 2 -> 3 -> None'
-    >>> linked_list_from_string('1 -> 2 -> 3 -> None')
-    Node(1, Node(2, Node(3)))
     """
     result = []
     splited_list = node_string.split(" -> ")
     rev_splited_list = splited_list[::-1]
-    print(rev_splited_list)
-    for el in rev_splited_list:
-        if el == "None":
-            result.append((Node(None, None)))
-        else:
+    for i, el in enumerate(rev_splited_list):
+        if rev_splited_list[i-1] == "None" and el != "None":
+            result.append((Node(int(el), None)))
+        elif el != "None":
             result.append(Node(int(el), result[-1]))
-    print(len(result))
+    if len(result) == 0:
+        return None
     return result[-1]
 
-print(linked_list_from_string('1 -> 2 -> 3 -> None'))
-
-
-def stringify(node):
-    """Stringify linked list
-    >>> stringify(Node(1, Node(2, Node(3))))
-    '1 -> 2 -> 3 -> None'
-    """
-    result = ""
-    if node is None:
-        return "None"
-    else:
-        probe = node
-        while probe is not None:
-            if probe.next is None:
-                result += f"{probe.data} -> None"
-            else:
-                result += f"{probe.data} -> "
-            probe = probe.next
-    return result
-
-
-print(stringify(linked_list_from_string('1 -> 2 -> 3 -> None')))
+# if __name__ == "__main__":
+#     import doctest
+#     doctest.testmod()
